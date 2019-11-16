d6a6dfa17a4d6bbf1914ba935f0813b5bfb6037a
miles marillo <mjm03@hampshire.edu>
2019-11-15 20:27:51 -0500

    basic frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+componentDidMount() {
+  alert('very nice')
 }
 
+render() {
+
+  const randomNumbah = () => {
+    let num = Math.floor(Math.random() * 10 + 1)
+    return <li>{num}</li>
+  }
+
+  return (
+    <div className="home">
+      welcome to the random numbers project, courtesy of mjm 2019
+      <br/><br/>
+    <h2>Current Number</h2>
+    {randomNumbah()}
+    <br/><br/>
+    <h3>Past Numbers</h3>
+    <ul>
+    {randomNumbah()}
+    {randomNumbah()}
+    {randomNumbah()}
+    </ul>
+    </div>
+  );
+  }
+}
 export default App;
