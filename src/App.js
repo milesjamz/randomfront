7c060fe0906aa158d83d5bafdb856875280f9da8
miles marillo <mjm03@hampshire.edu>
2019-11-18 11:54:25 -0500

    added lil bit

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import './App.css';
 class App extends React.Component {
 
 componentDidMount() {
-  alert('very nice')
-}
+  console.log('hehe')}
 
 render() {
 
   const randomNumbah = () => {
     let num = Math.floor(Math.random() * 10 + 1)
-    return <li>{num}</li>
+      let hmm = Date(Date.now())
+        let today = hmm.toString()
+    return <li>{today} - {num}</li>
   }
 
   return (
     <div className="home">
-      welcome to the random numbers project, courtesy of mjm 2019
+      <span className="yellow">welcome to the random numbers project -=-=-=-=- (c) MJM 2019</span>
       <br/><br/>
-    <h2>Current Number</h2>
+    <h2>Current Number</h2>    
     {randomNumbah()}
     <br/><br/>
-    <h3>Past Numbers</h3>
+    <h3>Past Numbers</h3>    
     <ul>
     {randomNumbah()}
     {randomNumbah()}
